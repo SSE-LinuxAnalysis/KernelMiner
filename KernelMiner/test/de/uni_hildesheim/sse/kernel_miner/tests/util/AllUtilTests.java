094520d4622cc302bb515b4a5b80565339a2abe2
Adam <adam@sse.uni-hildesheim.de>
2016-09-25 11:43:40 +0200

    Add FormulaTest

--- KernelMiner/test/de/uni_hildesheim/sse/kernel_miner/tests/util/AllUtilTests.java
+++ KernelMiner/test/de/uni_hildesheim/sse/kernel_miner/tests/util/AllUtilTests.java
@@ -4,11 +4,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import de.uni_hildesheim.sse.kernel_miner.tests.util.logic.FormulaTest;
 import de.uni_hildesheim.sse.kernel_miner.tests.util.parser.ParserTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({
     FilesTest.class,
+    FormulaTest.class,
     ParserTest.class,
     ZipArchiveTest.class,
 })
---
+++ KernelMiner/test/de/uni_hildesheim/sse/kernel_miner/tests/util/logic/FormulaTest.java
@@ -0,0 +1,128 @@
+package de.uni_hildesheim.sse.kernel_miner.tests.util.logic;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import de.uni_hildesheim.sse.kernel_miner.util.logic.Conjunction;
+import de.uni_hildesheim.sse.kernel_miner.util.logic.Disjunction;
+import de.uni_hildesheim.sse.kernel_miner.util.logic.False;
+import de.uni_hildesheim.sse.kernel_miner.util.logic.Formula;
+import de.uni_hildesheim.sse.kernel_miner.util.logic.Negation;
+import de.uni_hildesheim.sse.kernel_miner.util.logic.True;
+import de.uni_hildesheim.sse.kernel_miner.util.logic.Variable;
+
+public class FormulaTest {
+
+    @Test
+    public void testToStringComplex() {
+        Formula f = new Conjunction(
+                new Disjunction(new Variable("A"), new Negation(new True())),
+                new Conjunction(new Negation(new Variable("B")), new False())
+        );
+        
+        
+        Assert.assertEquals("((A || !1) && (!B && 0))", f.toString());
+    }
+    
+    @Test
+    public void testEvaluateSimpleVariable() {
+        Variable a = new Variable("A");
+        
+        a.setValue(true);
+        Assert.assertTrue(a.evaluate());
+        
+        a.setValue(false);
+        Assert.assertFalse(a.evaluate());
+    }
+    
+    @Test
+    public void testEvaluateSimpleConstants() {
+        Assert.assertTrue(new True().evaluate());
+        Assert.assertFalse(new False().evaluate());
+    }
+    
+    @Test
+    public void testEvaluateSimpleNegation() {
+        Variable a = new Variable("A");
+        Formula f = new Negation(a);
+        
+        a.setValue(true);
+        Assert.assertFalse(f.evaluate());
+        
+        a.setValue(false);
+        Assert.assertTrue(f.evaluate());
+    }
+    
+    @Test
+    public void testEvaluateSimpleConjunction() {
+        Variable a = new Variable("A");
+        Variable b = new Variable("B");
+        Formula f = new Conjunction(a, b);
+        
+        a.setValue(true);
+        b.setValue(true);
+        Assert.assertTrue(f.evaluate());
+        
+        a.setValue(true);
+        b.setValue(false);
+        Assert.assertFalse(f.evaluate());
+        
+        a.setValue(false);
+        b.setValue(true);
+        Assert.assertFalse(f.evaluate());
+        
+        a.setValue(false);
+        b.setValue(false);
+        Assert.assertFalse(f.evaluate());
+    }
+    
+    @Test
+    public void testEvaluateSimpleDisjunction() {
+        Variable a = new Variable("A");
+        Variable b = new Variable("B");
+        Formula f = new Disjunction(a, b);
+        
+        a.setValue(true);
+        b.setValue(true);
+        Assert.assertTrue(f.evaluate());
+        
+        a.setValue(true);
+        b.setValue(false);
+        Assert.assertTrue(f.evaluate());
+        
+        a.setValue(false);
+        b.setValue(true);
+        Assert.assertTrue(f.evaluate());
+        
+        a.setValue(false);
+        b.setValue(false);
+        Assert.assertFalse(f.evaluate());
+    }
+    
+    @Test
+    public void testEvaluateComplex() {
+        Variable a = new Variable("A");
+        Variable b = new Variable("B");
+        Formula f = new Conjunction(
+                new Disjunction(a, new Negation(new True())),
+                new Negation(b)
+        );
+        
+        a.setValue(true);
+        b.setValue(true);
+        Assert.assertFalse(f.evaluate());
+        
+        a.setValue(true);
+        b.setValue(false);
+        Assert.assertTrue(f.evaluate());
+        
+        a.setValue(false);
+        b.setValue(true);
+        Assert.assertFalse(f.evaluate());
+        
+        a.setValue(false);
+        b.setValue(false);
+        Assert.assertFalse(f.evaluate());
+    }
+
+}
