ed348f532c7d76b8d02f7152eb941316f689669c
Adam <adam@sse.uni-hildesheim.de>
2016-09-25 10:41:19 +0200

    Expand FilesTest

--- KernelMiner/test/de/uni_hildesheim/sse/kernel_miner/tests/util/FilesTest.java
+++ KernelMiner/test/de/uni_hildesheim/sse/kernel_miner/tests/util/FilesTest.java
@@ -2,6 +2,7 @@ package de.uni_hildesheim.sse.kernel_miner.tests.util;
 
 import java.io.ByteArrayInputStream;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.junit.Assert;
@@ -11,9 +12,59 @@ import de.uni_hildesheim.sse.kernel_miner.util.Files;
 
 public class FilesTest {
 
+    private static final File testdata = new File("testdata/FilesTest");
+    
     @Test
-    public void testReadFile1() throws IOException {
-        File toRead = new File("testdata/testReadFile1.txt");
+    public void testRelativizeWithExistingFiles() {
+        File dir = testdata;
+        File fileInDir = new File(dir.getPath() + "/existingFile.txt");
+        
+        Assert.assertEquals(dir.getPath().length() + 17, fileInDir.getPath().length());
+        
+        String relative = Files.relativize(fileInDir, dir);
+        
+        Assert.assertEquals("existingFile.txt", relative);
+    }
+    
+    @Test
+    public void testRelativizeWithNotExistingFiles() {
+        File dir = new File("doesntExist");
+        File fileInDir = new File(dir.getPath() + "/doesntExist.txt");
+        
+        Assert.assertEquals(dir.getPath().length() + 16, fileInDir.getPath().length());
+        
+        String relative = Files.relativize(fileInDir, dir);
+        
+        Assert.assertEquals("doesntExist.txt", relative);
+    }
+    
+    @Test
+    public void testRelativizeMultipleDirs() {
+        File dir = new File("doesntExist/a/b");
+        File fileInDir = new File(dir.getPath() + "/doesntExist.txt");
+        
+        Assert.assertEquals(dir.getPath().length() + 16, fileInDir.getPath().length());
+        
+        String relative = Files.relativize(fileInDir, dir.getParentFile());
+        
+        Assert.assertEquals("b/doesntExist.txt", relative);
+    }
+    
+    @Test
+    public void testRelativizeDotDot() {
+        File dir = new File("doesntExist/a/b");
+        File fileInDir = new File(dir.getPath() + "/../doesntExist.txt");
+        
+        Assert.assertEquals(dir.getPath().length() + 19, fileInDir.getPath().length());
+        
+        String relative1 = Files.relativize(fileInDir, dir.getParentFile());
+        
+        Assert.assertEquals("doesntExist.txt", relative1);
+    }
+    
+    @Test
+    public void testReadFileSmallText() throws IOException {
+        File toRead = new File(testdata, "testReadFile1.txt");
 
         String content = Files.readFile(toRead);
 
@@ -21,8 +72,8 @@ public class FilesTest {
     }
 
     @Test
-    public void testReadFile2() throws IOException {
-        File toRead = new File("testdata/testReadFile2.txt");
+    public void testReadFileBigText() throws IOException {
+        File toRead = new File(testdata, "testReadFile2.txt");
 
         String content = Files.readFile(toRead);
 
@@ -31,9 +82,21 @@ public class FilesTest {
 
         Assert.assertEquals(expected, content);
     }
+    
+    @Test
+    public void testReadFileNonExisting() throws IOException {
+        File toRead = new File(testdata, "doesntExist.txt");
+        
+        try {
+            Files.readFile(toRead);
+            Assert.fail("Expected exception");
+        } catch (FileNotFoundException e) {
+            
+        }
+    }
 
     @Test
-    public void testReadStream1() throws IOException {
+    public void testReadStreamShortText() throws IOException {
         String expected = "This is a test text";
         
         String content = Files.readFile(new ByteArrayInputStream(expected.getBytes()));
@@ -42,7 +105,7 @@ public class FilesTest {
     }
     
     @Test
-    public void testReadStream2() throws IOException {
+    public void testReadStreamBigText() throws IOException {
         String expected = "This is a longer text. It contains more than 512 characters, to check the case that more than one buffer needs to be used.\n";
         expected = expected + expected + expected + expected;
         
@@ -53,7 +116,7 @@ public class FilesTest {
     
     @Test
     public void testWriteFile() throws IOException {
-        File toWrite = new File("testdata/testWriteFile.txt");
+        File toWrite = new File(testdata, "testWriteFile.txt");
         if (toWrite.exists()) {
             toWrite.delete();
         }
@@ -64,6 +127,29 @@ public class FilesTest {
         
         Assert.assertTrue(toWrite.exists());
         Assert.assertEquals(content, Files.readFile(toWrite));
+        
+        toWrite.delete();
+    }
+    
+    @Test
+    public void testOverwriteFile() throws IOException {
+        File toWrite = new File(testdata, "testOverwriteFile.txt");
+        if (toWrite.exists()) {
+            toWrite.delete();
+        }
+        Assert.assertFalse(toWrite.exists());
+        
+        String content1 = "Text 1\n";
+        Files.writeFile(toWrite, content1);
+        
+        Assert.assertTrue(toWrite.exists());
+        Assert.assertEquals(content1, Files.readFile(toWrite));
+        
+        String content2 = "Text 2\n";
+        Files.writeFile(toWrite, content2);
+        
+        Assert.assertTrue(toWrite.exists());
+        Assert.assertEquals(content2, Files.readFile(toWrite));
         
         toWrite.delete();
     }
